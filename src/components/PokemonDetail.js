c9b27194c30dbcbd453b6692ca739c1c88ebee57
Xin Zhang <xinjohnzhang@gmail.com>
2019-09-18 16:12:27 -0700

    Add info to PokemonDetail

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -2,9 +2,46 @@ import React from 'react';
 
 class PokemonDetail extends React.Component {
   render() {
-    console.log(this.props.location)
+    const { name, pokemonInfo, imageUrl } = this.props.location.state;
+    const { height, weight, types, abilities } = pokemonInfo;
+    console.log(abilities)
     return (
-      <div className="pokemon-profile">
+      <div className="pokemon-detail">
+        <div className="pokemon-profile">
+          <img
+            src={imageUrl}
+            alt={name}
+          />
+          <p>{name}</p>
+          <ul>
+            <li key="height">Height {height}</li>
+            <li key="weight">Weight {weight}</li>
+          </ul>
+          <form>
+            <label htmlFor="inBag">In Bag</label>
+            <input 
+              type="checkbox" 
+              id="inBag" 
+              name="subscribe"
+            />
+          </form>
+          <ul>
+            {types.map(({ type }) => (
+              <li key={type.name}>{type.name}</li>
+            ))}
+          </ul>
+          <p>
+            This Pokémon is not satisfied unless it is rampaging at all times. If there is no opponent for it to battle, it will charge at thick trees and knock them down to calm itself.
+          </p>
+          <ul>
+            {abilities.map(({ ability }) => (
+              <li key={ability.name}>{ability.name}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="map-container">
+          
+        </div>
       </div>
     )
   }
