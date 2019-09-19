4ced6cb451e3cc3c2ae7bcb256b1b64d7e639a00
Xin Zhang <xinjohnzhang@gmail.com>
2019-09-18 18:12:25 -0700

    Add search input

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -2,6 +2,45 @@ import React from 'react';
 import PokemonPreview from './PokemonPreview';
 import { getPokemons } from '../utils/api';
 
+class PokedexInput extends React.Component {
+  state = {
+    value: '',
+  }
+
+  handleChange = (ev) => {
+    const value = ev.target.value;
+
+    this.setState({
+      value
+    })
+  }
+
+  handleSubmit = (ev) => {
+    ev.preventDefault();
+    const { value } = this.state;
+
+    this.props.onSubmit(value);
+  }
+
+  render() {
+    return (
+      <div className="form-container">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="search"
+            id="search"
+            placeholder="search"
+            autoComplete='off'
+            value={this.state.username}
+            onChange={this.handleChange}
+            size="30"
+          />
+        </form>
+      </div>
+    )
+  }
+}
+
 function PokemonGrid({ results }) {
   return (
     <ul className="pokemon-grid">
@@ -35,10 +74,19 @@ class Pokedex extends React.Component {
       });
   }
 
+    handleSearch = (value) => {
+    const { results } = this.state;
+
+    this.setState({
+      results : results.filter(({ name }) => name === value)
+    })
+  }
+
   render() {
     const { results } = this.state;
     return (
       <React.Fragment>
+        <PokedexInput onSubmit={this.handleSearch}/>
         {results && <PokemonGrid results={results} />}
       </React.Fragment>
     )
